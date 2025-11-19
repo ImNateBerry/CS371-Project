c4bb3f65d719dad5a6cb3379fdeed8e9f5b6d718
Nate Berry <145950810+ImNateBerry@users.noreply.github.com>
2025-11-19 18:54:52 -0500

    Updated pongServer.py to bind to port 12345

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -15,3 +15,46 @@ import threading
 # for each player and where the ball is, and relay that to each client
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
 # clients are and take actions to resync the games
+
+clients = []
+
+# =================================================================================================
+# Author: <Who wrote this method> 
+# Purpose: <What should this method do>
+# Pre: <What preconditions does this method expect to be true? Ex. This method expects the program to be in X state before being called> 
+# Post: <What postconditions are true after this method is called? Ex. This method changed global variable X to Y>
+# =================================================================================================
+def handle_clients(conn: socket.socket) -> None:
+
+    # initial width and height to the client
+    conn.send(b"640,480")
+
+    # recieve the data from client
+    while True: 
+        try:
+            data = conn.recv(1024)
+            if not data:
+                break
+            for c in clients:
+                if c != conn:
+                    c.send(data)
+        except:
+            break
+
+        conn.close()
+        if conn in clients:
+            clients.remove(conn)
+    
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.bind(("0.0.0.0", 12345))
+server.listen()
+
+print("Listening on port 12345...")
+
+while True:
+    conn, addr = server.accept()
+    clients.append(conn)
+    thread = threading.Thread(target=handle_client, args=(conn, addr), daemon=True)
+    thread.start()
